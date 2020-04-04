e51dbc5d7dd1c892e486e721f1b3e6efa36f2ad8
정햇살 <quf5616@naver.com>
2020-04-04 18:31:48 +0900

    pro_test

---
+++ app/src/main/java/com/example/project1/GPSparkActivity.java
@@ -0,0 +1,14 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GPSparkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_park_location);
+    }
+}
