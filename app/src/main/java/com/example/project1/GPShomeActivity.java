a6564c1a6fcfb4ac074feacb0c853e347659f852
정햇살 <quf5616@naver.com>
2020-04-05 18:58:28 +0900

    pro_test

--- app/src/main/java/com/example/project1/GPShomeActivity.java
+++ app/src/main/java/com/example/project1/GPShomeActivity.java
@@ -2,13 +2,31 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GPShomeActivity extends AppCompatActivity {
+
+
+    private Button now_lo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_home);
+
+
+
+        now_lo=findViewById(R.id.now_lo);
+        now_lo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(GPShomeActivity.this,GPSnowActivity.class);
+                startActivity(intent);//액티비티 이동
+
+            }
+        });
     }
 }
