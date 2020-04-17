67858ad08b87e0e9f79313451b993f2e453e0a6c
osc9556 <osc950506@gmail.com>
2020-04-17 14:04:33 +0900

    2020.04.17 14:04

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button login_button;
+    private Button findID_button;
+    private Button findPW_button;
+    private Button sign_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +26,33 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent =new Intent(MainActivity.this,MenuActivity.class);
                 startActivity(intent);//액티비티 이동
+            }
+        });
 
+        findID_button = findViewById(R.id.findID_button);
+        findID_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FindIdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findPW_button = findViewById(R.id.findPW_button);
+        findPW_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FindPassActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sign_button = findViewById(R.id.sign_button);
+        sign_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignActivity.class);
+                startActivity(intent);
             }
         });
     }
