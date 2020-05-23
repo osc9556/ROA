290ed40f354fa2b86474e6037e28889f66a4df28
sunny <quf5616@naver.com>
2020-05-23 21:30:21 +0900

    ㅗㅕ

--- app/src/main/java/com/example/project1/SignActivity.java
+++ app/src/main/java/com/example/project1/SignActivity.java
@@ -1,12 +1,15 @@
 package com.example.project1;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,109 +22,104 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 public class SignActivity extends AppCompatActivity {
 
     private Button sign_button;
-
-    private ArrayAdapter adapter;
-    private Spinner spinner;
-    private String userID;
-    private String userPassword;
-    private String userName;
-    private String userPhonenum;
-    private String userEmail;
     private AlertDialog dialog;
-    private boolean vaildate=false;
+    private EditText editTextId;
+    private EditText editTextPw;
+    private EditText editTextName;
+    private EditText editTextphone;
+    private EditText editTextEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
-        final EditText idtext = (EditText) findViewById(R.id.idtext);
-        final EditText pwtext = (EditText) findViewById(R.id.pwtext);
-        final EditText nametext = (EditText) findViewById(R.id.nametext);
-        final EditText phtext = (EditText) findViewById(R.id.phtext);
-        final EditText etext = (EditText) findViewById(R.id.etext);
+        editTextId = (EditText) findViewById(R.id.idtext);
+        editTextPw = (EditText) findViewById(R.id.pwtext);
+        editTextName=(EditText) findViewById(R.id.nametext);
+        editTextphone=(EditText) findViewById(R.id.phtext);
+        editTextEmail= (EditText) findViewById(R.id.etext);
 
 
-
-
-        Button sign_button=(Button) findViewById(R.id.sign_button);
-        sign_button.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-
-                String userID=idtext.getText().toString();
-                String userPassword=pwtext.getText().toString();
-                String userName=nametext.getText().toString();
-                String userPhonenum =phtext.getText().toString();
-                String userEmail=etext.getText().toString();
-
-                if (userID.equals("") || userPassword.equals(""))
-                {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(SignActivity.this);
-                    dialog = builder.setMessage("아이디와 비밀번호는 필수 입력입니다")
-                            .setNegativeButton(("확인"), null)
-                            .create();
-                    dialog.show();
-                    return;
-                }
-
-
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-
-                            if (success)
-                            {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignActivity.this);
-                                dialog = builder.setMessage("회원등록에 성공했습니다")
-                                        .setPositiveButton("확인", null)
-                                        .create();
-                                dialog.show();
-                                finish();
-                            }
-                            else
-
-                            {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignActivity.this);
-                                dialog = builder.setMessage("회원등록에 실패했습니다")
-                                        .setNegativeButton(("확인"), null)
-                                        .create();
-                                dialog.show();
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-
-                        }
-                    }
-
-
-                };
-                RegisterRequest registerRequest=new RegisterRequest(userID,userPassword,userName,userPhonenum,userEmail,responseListener);
-                RequestQueue queue= Volley.newRequestQueue(SignActivity.this);
-                queue.add(registerRequest);
-
-            }
-
-
-        });
     }
-    protected  void onStop(){
-        super.onStop();
-        if(dialog!=null)
-        {
-            dialog.dismiss();
-            dialog=null;
 
+    public void insert(View view) {
+        String Id = editTextId.getText().toString();
+        String Pw = editTextPw.getText().toString();
+        String Name = editTextName.getText().toString();
+        String Phone = editTextphone.getText().toString();
+        String Email = editTextEmail.getText().toString();
+
+
+        insertoToDatabase(Id, Pw, Name, Phone, Email);
+    }
+    private void insertoToDatabase(String Id, String Pw, String Name, String Phone, String Email) {
+        class InsertData extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(SignActivity.this, "Please Wait", null, true, true);
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String Id = (String) params[0];
+                    String Pw = (String) params[1];
+                    String Name = (String) params[2];
+                    String Phone = (String) params[3];
+                    String Email = (String) params[4];
+
+                    String link = "http://15.164.233.187/phpmyadmin/usersign.php";
+                    String data = URLEncoder.encode("Id", "UTF-8") + "=" + URLEncoder.encode(Id, "UTF-8");
+                    data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                    data += "&" + URLEncoder.encode("Name", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                    data += "&" + URLEncoder.encode("Phone", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                    data += "&" + URLEncoder.encode("Email", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
         }
+        InsertData task = new InsertData();
+        task.execute(Id, Pw ,Name,Phone,Email);
     }
 }
-
